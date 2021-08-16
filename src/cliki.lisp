3adcc71e8b67212a315cd2e0e5cd251623a65d26
Eitaro Fukamachi <e.arrows@gmail.com>
2021-08-17 08:53:21 +0900

    Revert "Make the interval longer."
    This reverts commit 7512f10163eee575210ca25c4cd699540fb8204c.

--- src/cliki.lisp
+++ src/cliki.lisp
@@ -62,7 +62,7 @@
                  "#content #article .category" (text))))
         :test #'string=))))
 
-(defun generate-cliki-topics (out release-names &key (interval 10))
+(defun generate-cliki-topics (out release-names &key (interval 5))
   (let ((all-count (length release-names)))
     (format out "~&{~%")
     (loop for i from 1
